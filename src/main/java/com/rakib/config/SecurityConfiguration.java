6bcce7b4dc5676419626737c66a3ae21c1039e3a
rakib <jhadul.islam@itconquest.com>
2020-09-01 21:44:50 +0600

    add

--- src/main/java/com/rakib/config/SecurityConfiguration.java
+++ src/main/java/com/rakib/config/SecurityConfiguration.java
@@ -34,7 +34,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .cors().disable()
                 .authorizeRequests()
-                .antMatchers("/api/v1/**").permitAll()
+                .antMatchers("/api/v1/**","/").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
